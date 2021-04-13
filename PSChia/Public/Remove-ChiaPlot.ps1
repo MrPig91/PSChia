9875329fc9acc83fa11e08b6af8d90db9e9b4c35
MrPig91 <sirius.cleveland@gmail.com>
2021-04-12 20:47:15 -0500

    Added New Public Function Remove-ChiaPlot (Not Tested)

---
+++ PSChia/Public/Remove-ChiaPlot.ps1
@@ -0,0 +1,22 @@
+function Remove-ChiaPlot {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [Alias("FullName")]
+        [string[]]$FileName
+    )
+
+    Process{
+        foreach ($plot in $FileName){
+            $Param = @{
+                Command = "delete_plot"
+                Parameters = (@{filename = $plot} | ConvertTo-Json)
+                Service = "Harvester"
+            }
+
+            Write-Warning "Function not tested yet and so not yet implemented"
+            #$response = Invoke-chiaRPCCommand @Param
+            #$response
+        }
+    }
+}
