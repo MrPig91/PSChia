6907ea2c5e2be154cdb563fe170c62294882ed59
MrPig91 <sirius.cleveland@gmail.com>
2021-04-15 00:59:20 -0500

    New Public Function Remove-ChiaPlotDirectory

---
+++ PSChia/Public/Remove-ChiaPlotDirectory.ps1
@@ -0,0 +1,23 @@
+function Remove-ChiaPlotDirectory {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string[]]$Path
+    )
+
+    Process{
+        foreach ($directory in $Path){
+            $Param = @{
+                Command = "remove_plot_directory"
+                Parameters = (@{dirname = $directory} | ConvertTo-Json)
+                Service = "Harvester"
+            }
+            Invoke-chiaRPCCommand @Param | foreach {
+                [PSCustomObject]@{
+                    Path = $directory
+                    Success = $_.success
+                }
+            }
+        }
+    }
+}
