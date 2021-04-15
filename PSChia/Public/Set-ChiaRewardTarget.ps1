bedc984f539ee69b88f374712248bea95d33e3c2
MrPig91 <sirius.cleveland@gmail.com>
2021-04-15 00:59:36 -0500

    New Public Function Set-ChiaRewardTarget

---
+++ PSChia/Public/Set-ChiaRewardTarget.ps1
@@ -0,0 +1,18 @@
+function Set-ChiaRewardTarget {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$FarmerTargetAddress,
+
+        [Parameter(Mandatory)]
+        [string]$PoolTargetAddress
+    )
+
+    $Param = @{
+        Command = "get_reward_targets"
+        Parameters = @{farmer_target = $FarmerTargetAddress; pool_target = $PoolTargetAddress} | ConvertTo-Json
+        Service = "Farmer"
+    }
+
+    Invoke-chiaRPCCommand @Param
+}
