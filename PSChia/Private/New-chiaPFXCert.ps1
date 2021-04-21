28f3062c03f00128b1f5960df19a8e42d5005122
MrPig91 <sirius.cleveland@gmail.com>
2021-04-20 23:32:28 -0500

    Added HostName Parameter

--- PSChia/Private/New-chiaPFXCert.ps1
+++ PSChia/Private/New-chiaPFXCert.ps1
@@ -1,15 +1,22 @@
-function New-chiaPFXCert{
+function New-chiaPFXCert {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [ValidateSet("Harvester","Wallet","Full_Node","Farmer")]
-        [string]$Service
+        [string]$Service,
+        [string]$HostName = "Localhost",
+        [string]$CertPathDirectory
     )
     Write-Information "Creating new PFX Cert for $Service service"
     $ErrorActionPreference = "Stop"
     try{
         $OpenSSL = Get-OpenSSLPath -ErrorAction Stop
-        $ServicePath = "$ENV:USERPROFILE\.Chia\mainnet\config\ssl\$Service"
+        if ($PSBoundParameters.ContainsKey("CertPathDirectory")){
+            $ServicePath = $CertPathDirectory
+        }
+        else{
+            $ServicePath = "$ENV:USERPROFILE\.Chia\mainnet\config\ssl\$Service"
+        }
         $PFXPath = "$ENV:LOCALAPPDATA\PSChia"
         if (!(Test-Path $PFXPath)){
             New-Item -Path "$ENV:LOCALAPPDATA\PSChia" -ItemType Directory | Out-Null
@@ -29,12 +36,17 @@ function New-chiaPFXCert{
             $PSCmdlet.ThrowTerminatingError($ErrorRecord)
         }
 
+        $PFXHostPath = "$ENV:LOCALAPPDATA\PSChia\$Hostname"
+        if (!(Test-Path $PFXHostPath)){
+            New-Item -Path "$ENV:LOCALAPPDATA\PSChia\$Hostname" -ItemType Directory | Out-Null
+        }
+
         $pass = New-chiaPassword
         $password = ConvertTo-SecureString -String $pass -AsPlainText -Force
         $EncryptedPassword = ConvertFrom-SecureString -SecureString $password
-        $EncryptedPassword | Out-File -FilePath "$ENV:LOCALAPPDATA\PSChia\$($Service)Pass.txt"
+        $EncryptedPassword | Out-File -FilePath "$PFXHostPath\$($Service)Pass.txt"
     
-        &$OpenSSL pkcs12 -export -out "$ENV:LOCALAPPDATA\PSChia\$($Service).pfx" -inkey $key -in $cert -passout pass:$pass
+        &$OpenSSL pkcs12 -export -out "$PFXHostPath\$($Service).pfx" -inkey $key -in $cert -passout pass:$pass
         Write-Information "$Service PFX Cert successfully created"
         $ErrorActionPreference = "Continue"
     }
